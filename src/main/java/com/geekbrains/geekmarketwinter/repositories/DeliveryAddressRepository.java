8c92e8d0abcab4debf5a615a8db065760d89621f
ligovean <ligomail@rambler.ru>
2020-06-14 00:31:43 +0300

    Complite

---
+++ src/main/java/com/geekbrains/geekmarketwinter/repositories/DeliveryAddressRepository.java
@@ -0,0 +1,8 @@
+package com.geekbrains.geekmarketwinter.repositories;
+
+import com.geekbrains.geekmarketwinter.entites.DeliveryAddress;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeliveryAddressRepository extends CrudRepository<DeliveryAddress, Long> {
+
+}
